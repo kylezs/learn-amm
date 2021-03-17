d8ed26269adb56d95dedfc8144715cb0722e52d8
Kyle Zsembery <zsembery.kyle@gmail.com>
2021-03-17 18:17:28 +1100

    add antd, fresh landing page

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Learn AMM renders', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Learn AMM/i);
   expect(linkElement).toBeInTheDocument();
 });
