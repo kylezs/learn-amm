a4ecbc9ce20bd3cf4a57715e32c486365f32fee1
Harris <hjubb@users.noreply.github.com>
2021-03-21 13:47:21 +1100

    fix: swap syncWebAssembly for asyncWebAssembly and uncommenting the wasm import

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 // import './App.css';
 import  {Button} from 'antd';
 
-// import { add_two_ints } from 'wasm';
+import { add_two_ints } from 'wasm';
 
 function App() {
    return (
       <div>
          <h1>Hello</h1>
-         {/* <p>{add_two_ints(3, 4)}</p> */}
+          <p>{add_two_ints(3, 4)}</p>
       </div>
    );
 }
--- react/webpack.config.js
+++ react/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
     },
     experiments: {
-        syncWebAssembly: true
+        asyncWebAssembly: true
     },
     module: {
         rules: [
