3535da13398c1946bbb7b9c0d7c5020e6519be88
Kyle Zsembery <zsembery.kyle@gmail.com>
2021-03-22 19:21:23 +1100

    dynamic add 2

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,15 +1,26 @@
-import React from 'react';
+import { ChangeEvent, useState } from 'react';
 // import './App.css';
 import  {Button} from 'antd';
 
 import { add_two_ints } from 'wasm';
 
 function App() {
+
+   const [inAmount, setInAmount] = useState(0);
+
+   const onInputInAmount = (event: ChangeEvent<HTMLInputElement>) => {
+      const val = event.target.value;
+      const floatVal = Number.parseFloat(val);
+      setInAmount(floatVal);
+   }
+
    return (
       <div>
          <h1>Hello</h1>
-          <p>{add_two_ints(3, 4)}</p>
-      </div>
-   );
-}
+         <input type="number" value={inAmount} onChange={(e) => onInputInAmount(e)}></input>
+         <p>{inAmount}</p>
+          <p>{add_two_ints(inAmount, 4)}</p>
+          </div>
+         )
+      }
 export default App;
