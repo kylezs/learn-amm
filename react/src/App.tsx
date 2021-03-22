9bc2770f6ed80afd4067b6431783eb4de3776219
Kyle Zsembery <zsembery.kyle@gmail.com>
2021-03-22 21:23:17 +1100

    dynamic pool depths

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -7,22 +7,44 @@ import { swap, Pools } from 'wasm';
 function App() {
 
    const [inAmount, setInAmount] = useState(0);
+   const [depthX, setDepthX] = useState(0);
+   const [depthY, setDepthY] = useState(0);
 
-   const onInputInAmount = (event: ChangeEvent<HTMLInputElement>) => {
+   const onChangeInAmount = (event: ChangeEvent<HTMLInputElement>) => {
       const val = event.target.value;
       const intVal = Number.parseInt(val);
-      setInAmount(intVal);
+      if (!isNaN(intVal)) {
+         setInAmount(intVal);
+      }
+   }
+
+   
+   const onChangeDepthXAmount = (event: ChangeEvent<HTMLInputElement>) => {
+      const val = event.target.value;
+      const intVal = Number.parseInt(val);
+      if (!isNaN(intVal)) {
+         setDepthX(intVal);
+      }
+   }
+
+   const onChangeDepthYAmount = (event: ChangeEvent<HTMLInputElement>) => {
+      const val = event.target.value;
+      const intVal = Number.parseInt(val);
+      if (!isNaN(intVal)) {
+         setDepthY(intVal);
+      }
    }
 
    const pools = Pools.new(BigInt(500), BigInt(10));
 
    return (
       <div>
-         <h1>Hello</h1>
-         <input type="number" value={inAmount} onChange={(e) => onInputInAmount(e)}></input>
-         <p>{inAmount}</p>
-          <p>{swap(BigInt(inAmount), Pools.new(BigInt(500), BigInt(10)))}</p>
-          </div>
+         <h1>Learn AMM</h1>
+         PoolDepthX: <input type="number" value={depthX} onChange={(e) => onChangeDepthXAmount(e)}></input>
+         PoolDepthY: <input type="number" value={depthY} onChange={(e) => onChangeDepthYAmount(e)}></input>
+         InputAmount: <input type="number" value={inAmount} onChange={(e) => onChangeInAmount(e)}></input>
+          <p>{swap(BigInt(inAmount), Pools.new(BigInt(depthX), BigInt(depthY)))}</p>
+      </div>
          )
       }
 export default App;
